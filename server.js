ea040d57abd7e32d67a27a99a3de2749d503e574
Mohit Kumar <mohit.kumar08@nagarro.com>
2019-04-25 15:59:30 +0530

    name changed

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const express = require('express');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/find-weather'));
+app.use(express.static(__dirname + '/dist/findWeather'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(__dirname+'/dist/find-weather/index.html');
+res.sendFile(__dirname+'/dist/findWeather/index.html');
 });
 
 // Start the app by listening on the default Heroku port
